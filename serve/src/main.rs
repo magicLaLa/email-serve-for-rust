61daadd9f2a1ce1c0c9108184807f849e5697529
duolong <tomato.stao@gmail.com>
2022-06-28 20:05:25 +0800

    :fire: remove Useless output

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -47,9 +47,6 @@ async fn main() {
         Err(_) => PASS_WORD.to_string(),
     };
 
-    println!("user_name : {}", user_name);
-    println!("pasword: {}", pasword);
-
     if user_name.is_empty() || pasword.is_empty() {
         panic!("USER or PASS_WORD is Required !");
     }
